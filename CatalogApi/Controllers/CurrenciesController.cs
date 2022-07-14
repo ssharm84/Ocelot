f411368cd53f07d1d64474bd13b55ca3906767ac
Surinder <er.surindersharma2012@gmail.com>
2022-07-14 22:26:02 +0530

    Add project files.

---
+++ CatalogApi/Controllers/CurrenciesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogApi.Controllers
+{
+    [ApiController]
+    [Route("api/currencies")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet,Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"usd","inr", "jpy"};
+        }
+    }
+}
---
+++ OrdersApi/Controllers/OrdersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrdersApi.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersController : ControllerBase
+    {
+       [HttpGet,Route("")]
+        public IActionResult GetOrders()
+        {
+            var o1 = new Order{Id="101",Amount=200};
+            var o2 = new Order{Id="102",Amount=400};
+            return Ok(new List<Order>{o1,o2});
+        }
+
+    }
+
+    public class Order
+    {
+        public string Id { get; set; }
+        public decimal Amount { get; set; }
+
+        public Order(){}
+        public Order(string id, decimal amount)
+        {
+            Id = id;
+            Amount = amount;
+        }
+    }
+}
